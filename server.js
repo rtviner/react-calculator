083e9243788b5f457926ad522fbf15dfd4b4e8ce
Rebecca Viner <rtviner@gmail.com>
2019-06-20 19:40:25 -0600

    update express server to look to the build folder

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-import express from 'express';
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + '/'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname + '/build'));
 
-app.get('*', (req, res) =>{
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
-
-app.listen(port, () => {
-	console.log('listening on port ', server.address().port);
-});
+app.listen(port);
